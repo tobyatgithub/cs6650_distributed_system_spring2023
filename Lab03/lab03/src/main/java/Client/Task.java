2d704bb70bd6ae4f42cd133bc4760d29e965a6ed
TobyAtStaffbase <fangyuan.huang@staffbase.com>
2023-02-04 16:20:39 -0800

    lab03: using an array trying to record the performance metrcs. Not a good idea when using newFixedThreadPool.

--- Lab03/lab03/src/main/java/Client/Task.java
+++ Lab03/lab03/src/main/java/Client/Task.java
@@ -1,31 +1,41 @@
 package Client;
 
-import io.swagger.client.ApiClient;
 import io.swagger.client.ApiException;
 import io.swagger.client.api.SwipeApi;
 import io.swagger.client.model.SwipeDetails;
-import org.apache.commons.httpclient.*;
-import org.apache.commons.httpclient.methods.*;
-import org.apache.commons.httpclient.params.HttpMethodParams;
 
-import java.io.IOException;
+import java.util.Hashtable;
 import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Task implements Runnable {
+    private boolean PRINT;
     private final SwipeApi apiInstance;
+    private final Hashtable<String, String[]> recordTable;
     private int iterationNum;
-    private Metrics metrics;
+    private AtomicInteger failRequestCounter;
 
-    public Task(int iterationNum, SwipeApi apiInstance, Metrics metrics) {
+    public Task(int iterationNum, SwipeApi apiInstance, Hashtable<String, String[]> recordTable, AtomicInteger failRequestCounter, boolean PRINT) {
         this.iterationNum = iterationNum;
-        this.metrics = metrics;
         this.apiInstance = apiInstance;
+        this.recordTable = recordTable;
+        this.failRequestCounter = failRequestCounter;
+        this.PRINT = PRINT;
     }
 
 
     @Override
     public void run() {
-        System.out.println("Running task... " + iterationNum);
+        long start = System.currentTimeMillis();
+        long threadId = Thread.currentThread().getId();
+
+        System.out.println("Thread ID = " + threadId);
+
+        recordTable.get("startTime")[(int) threadId] += Long.toString(start) + ", ";
+        recordTable.get("responseType")[(int) threadId] += "POST, ";
+        if(PRINT) {
+            System.out.println("Running task... " + iterationNum);
+        }
         String leftOrRight = new Random().nextBoolean() ? "left" : "right";
         String randomSwiperId = String.valueOf(new Random().nextInt(5000 - 1) + 1);
         String randomSwipeeId = String.valueOf(new Random().nextInt(1000000 - 1) + 1);
@@ -41,16 +51,23 @@ public class Task implements Runnable {
         while (true) {
             try {
                 apiInstance.swipe(body, leftOrRight);
-                System.out.println(body);
-                return;
+                if (PRINT) {
+                    System.out.println(body);
+                }
+                recordTable.get("responseCode")[(int) threadId] += "200, ";
+                break;
             } catch (ApiException e) {
                 if (++count >= maxCount) {
-//                    count = 0;
+                    failRequestCounter.getAndIncrement();
                     System.err.println("Exception when calling SwipeApi#swipe");
                     e.printStackTrace();
-                    return;
+                    recordTable.get("responseCode")[(int) threadId] += "404, ";
+                    break;
                 }
             }
         }
+        long finish = System.currentTimeMillis();
+        long timeElapsed = finish - start;
+        recordTable.get("latency")[(int) threadId] += timeElapsed + ", ";
     }
 }
--- Lab03/lab03/src/main/java/Client/TwinderClient.java
+++ Lab03/lab03/src/main/java/Client/TwinderClient.java
@@ -3,37 +3,45 @@ package Client;
 import io.swagger.client.ApiClient;
 import io.swagger.client.api.SwipeApi;
 
-import java.util.List;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Hashtable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.Map.Entry;
+import java.util.Set;
 
 public class TwinderClient {
-    private static final int NUM_OF_THREADS = 200;
+    private static final int NUM_OF_THREADS = 10;
+    private static final int NUM_OF_TASKS = 1_000;
+    private static final boolean PRINT = false;
+
+    private static Hashtable<String, String[]> recordTable = new Hashtable<String, String[]>() {{
+        put("startTime", new String[NUM_OF_THREADS]);
+        put("responseType", new String[NUM_OF_THREADS]);
+        put("latency", new String[NUM_OF_THREADS]);
+        put("responseCode", new String[NUM_OF_THREADS]);
+    }};
+
+
+    static AtomicInteger failRequestCounter = new AtomicInteger(0);
     static private String url = "http://18.236.26.147:8080/lab03_war/TwinderAPI/";
-//    static private String url = "http://54.184.114.172:8080/JavaServlet_war/twinder";
+    // (500k,200) = 197741 ms; (1k, 1) = 49514 ms
+    // static private String url = "http://54.184.114.172:8080/JavaServlet_war/twinder";
     // nick's endpoint, 1-28-2023 working!
-    private static Metrics metricsTracker;
     private int iterationNum;
     private static final ExecutorService executorService = Executors.newFixedThreadPool(NUM_OF_THREADS);
-    public TwinderClient(Metrics metricsTracker, int iterationNum, List<Metrics> metricsList) {
-        this.iterationNum = iterationNum;
-        this.metricsTracker = metricsTracker;
-    }
-
-    public static Metrics getMetricsTracker() {
-        return metricsTracker;
-    }
 
     public static void main(String[] args) {
-        System.out.println("hello.");
-        int NUM_OF_CLIENTS = 500_000;
         long start = System.currentTimeMillis();
-        for (int i = 0; i < NUM_OF_CLIENTS; i++) {
+        for (int i = 0; i < NUM_OF_TASKS; i++) {
             ApiClient myClient = new ApiClient();
             myClient.setBasePath(url);
             SwipeApi apiInstance = new SwipeApi(myClient);
-            executorService.submit(new Task(i, apiInstance, metricsTracker));
+            executorService.submit(new Task(i, apiInstance, recordTable, failRequestCounter, PRINT));
         }
         executorService.shutdown();
         try {
@@ -44,7 +52,25 @@ public class TwinderClient {
         long finish = System.currentTimeMillis();
         long timeElapsed = finish - start;
         System.out.println("Time spent: " + timeElapsed + " milliseconds");
-        System.out.println("Request made: ");
-        System.out.println(metricsTracker);
+        System.out.println("Number of request made: " + NUM_OF_TASKS);
+        System.out.println("Number of fail request: " + failRequestCounter.get());
+//        System.out.println(recordTable);
+        // Write record table values to a csv
+        Set<Entry<String, String[]>> entrySet = recordTable.entrySet();
+        StringBuilder sb = new StringBuilder();
+        for (Entry<String, String[]> entry : entrySet) {
+            sb.append(entry.getKey());
+            sb.append(",");
+            sb.append(Arrays.toString(entry.getValue()));
+            sb.append("\n");
+        }
+
+        try (FileWriter writer = new FileWriter("hashtable.csv")) {
+            writer.write(sb.toString());
+            System.out.println("The contents of the hashtable have been written to hashtable.csv");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
 }
