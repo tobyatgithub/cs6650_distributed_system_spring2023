e3758fc56b38ce4aece30a989a5f445a36636a2c
TobyAtStaffbase <fangyuan.huang@staffbase.com>
2023-02-19 01:14:31 -0800

    lab06: cache. v1 for api finished. tested local ok.

---
+++ Lab06_buildServer/lab06/src/main/java/neu/cs6650/Client/TwinderClient.java
@@ -0,0 +1,57 @@
+package neu.cs6650.Client;
+
+import io.swagger.client.ApiClient;
+import io.swagger.client.api.SwipeApi;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class TwinderClient {
+    private static final int NUM_OF_THREADS = 200;
+    private static final int NUM_OF_TASKS = 500_000;
+    private static final boolean PRINT = false;
+    private static final String CSV_FILENAME = "lab06_performance_(" + NUM_OF_THREADS + "_" + NUM_OF_TASKS + ").csv";
+
+    static AtomicInteger failRequestCounter = new AtomicInteger(0);
+    static private String url = "http://18.236.26.147:8080/lab06_war/TwinderAPI/";
+    // (500k,200) = 197741 ms; (1k, 1) = 49514 ms
+    // static private String url = "http://54.184.114.172:8080/JavaServlet_war/twinder";
+    // nick's endpoint, 1-28-2023 working!
+    private int iterationNum;
+    private static final ExecutorService executorService = Executors.newFixedThreadPool(NUM_OF_THREADS);
+
+    public static void main(String[] args) throws IOException {
+        FileWriter writer = new FileWriter(CSV_FILENAME, false);
+        writer.append("start time, request type, latency, response code\n");
+
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < NUM_OF_TASKS; i++) {
+            ApiClient myClient = new ApiClient();
+            myClient.setBasePath(url);
+            SwipeApi apiInstance = new SwipeApi(myClient);
+            executorService.submit(new Task(i, apiInstance, failRequestCounter, PRINT, writer));
+        }
+        executorService.shutdown();
+        try {
+            executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        long finish = System.currentTimeMillis();
+        long timeElapsed = finish - start;
+        System.out.println("Time spent: " + timeElapsed + " milliseconds");
+        System.out.println("Number of request made: " + NUM_OF_TASKS);
+        System.out.println("Number of fail request: " + failRequestCounter.get());
+        writer.append("Total time spend, thread, total request, fail request\n");
+        writer.append(String.valueOf(timeElapsed)).append("ms, ");
+        writer.append(NUM_OF_THREADS + ", ");
+        writer.append(NUM_OF_TASKS + ", ");
+        writer.append(String.valueOf(failRequestCounter.get()));
+        writer.close();
+    }
+
+}
---
+++ Lab06_buildServer/lab06/src/main/java/neu/cs6650/TwinderAPI.java
@@ -0,0 +1,151 @@
+package neu.cs6650;
+
+import com.google.gson.Gson;
+import com.rabbitmq.client.*;
+import io.swagger.client.JSON;
+import io.swagger.client.model.SwipeDetails;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+import org.apache.commons.pool2.BasePooledObjectFactory;
+import org.apache.commons.pool2.ObjectPool;
+import org.apache.commons.pool2.PooledObject;
+import org.apache.commons.pool2.impl.DefaultPooledObject;
+import org.apache.commons.pool2.impl.GenericObjectPool;
+
+@WebServlet(name = "TwinderAPI", value = "/TwinderAPI/*")
+public class TwinderAPI extends HttpServlet {
+    private static final String RPC_QUEUE_NAME = "rpc_queue";
+    private static final String RabbitMQ_URL = "localhost";
+    private ObjectPool<Channel> channelPool;
+
+    private Gson gson;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        gson = new Gson();
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost(RabbitMQ_URL);
+        Connection connection;
+        try {
+            connection = factory.newConnection();
+            channelPool = new GenericObjectPool<>(new ChannelFactory(connection));
+        } catch (TimeoutException | IOException e) {
+            throw new ServletException(e);
+        }
+    }
+
+    private static class ChannelFactory extends BasePooledObjectFactory<Channel> {
+
+        private final Connection connection;
+
+        ChannelFactory(Connection connection) {
+            this.connection = connection;
+        }
+
+        @Override
+        public Channel create() throws Exception {
+            return connection.createChannel();
+        }
+
+        @Override
+        public PooledObject<Channel> wrap(Channel channel) {
+            return new DefaultPooledObject<>(channel);
+        }
+
+        @Override
+        public void destroyObject(PooledObject<Channel> p) throws Exception {
+            Channel channel = p.getObject();
+            if (channel.isOpen()) {
+                channel.close();
+            }
+        }
+    }
+
+    private boolean urlIsValid(String urlPath, HttpServletResponse response) throws IOException {
+        // expect: /swipe/{leftorright}/ = [, swipe, left]
+        String[] urlParts = urlPath.split("/");
+        if (urlPath.isEmpty()) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            response.getWriter().write("empty link");
+            return false;
+        }
+        if ((urlParts.length != 3) ||
+                (!urlParts[1].matches("swipe")) ||
+                (urlParts[2].matches("left") || urlParts[2].matches("right"))) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            response.getWriter().write("missing parameters!");
+            return false;
+        }
+        return true;
+    }
+
+    private boolean jsonIsValid(StringBuilder sBuilder, HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String line;
+        try {
+            while ((line = request.getReader().readLine()) != null) {
+                sBuilder.append(line);
+            }
+            gson.fromJson(sBuilder.toString(), SwipeDetails.class);
+            response.setStatus(HttpServletResponse.SC_OK);
+            response.getWriter().write("Successfully received a swipe.");
+            return true;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String urlPath = request.getPathInfo();
+        if (!urlIsValid(urlPath, response)) {
+            System.out.println("FAIL: didn't pass url validation.");
+            return;
+        }
+        // validate json
+        StringBuilder sBuilder = new StringBuilder();
+        if (!jsonIsValid(sBuilder, request, response)) {
+            System.out.println("FAIL: didn't pass json validation.");
+            return;
+        }
+
+        Channel channel = null;
+        try {
+            channel = channelPool.borrowObject();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        channel.queueDeclare(RPC_QUEUE_NAME, false, false, false, null);
+        channel.queuePurge(RPC_QUEUE_NAME);
+        channel.basicQos(1);
+        System.out.println(" [x] Awaiting RPC requests");
+        String contentString = gson.fromJson(sBuilder.toString(), JSON.class).toString();
+        channel.basicPublish("", RPC_QUEUE_NAME, null, contentString.getBytes());
+        Channel finalChannel = channel;
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            AMQP.BasicProperties replyProps = new AMQP.BasicProperties
+                    .Builder()
+                    .correlationId(delivery.getProperties().getCorrelationId())
+                    .build();
+
+            String resultString = "";
+            try {
+                String message = new String(delivery.getBody(), "UTF-8");
+                System.out.println("message: " + message + ")");
+            } catch (Exception e) {
+                System.out.println("error: " + e);
+            } finally {
+                finalChannel.basicPublish("", delivery.getProperties().getReplyTo(), replyProps, resultString.getBytes("UTF-8"));
+                finalChannel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+            }
+        };
+
+        channel.basicConsume(RPC_QUEUE_NAME, false, deliverCallback, (consumerTag -> {
+        }));
+    }
+}
